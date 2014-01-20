965aa3f83e9beac889f0620f939f1f063aff634d
David Blanchet <david.blanchet5@gmail.com>
2014-01-20 17:57:41 +0100

    GUI: Simplify flight description code
    Also add i18n capability to description.

--- gui/util.py
+++ gui/util.py
@@ -1,4 +1,7 @@
+import gettext
+_ = gettext.translation('messages', 'gui', fallback=True).ugettext
+
+
 def flight_description(flight):
-    duration = flight.records[-1].time + 1.0 / flight.sampling_freq
-    return '%3.3d - %10.3f secs @ %d Hz' % \
-                (flight.index, duration, flight.sampling_freq)
+    return _('%3.3d - %10.3f secs @ %d Hz') % \
+                (flight.index, flight.duration, flight.sampling_freq)
