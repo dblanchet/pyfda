18ecd52c8285b508649611c904f2c33dab4d9539
David Blanchet <david.blanchet5@gmail.com>
2014-02-13 22:26:46 +0100

    GUI: Add Altimeter Control action button
    No action is implemented yet.

--- gui/fdaaltimetercontrol.py
+++ gui/fdaaltimetercontrol.py
@@ -2,6 +2,8 @@
 
 import Tkinter as tk
 
+import tkMessageBox
+
 import gettext
 _ = gettext.translation('messages', 'gui', fallback=True).ugettext
 
@@ -19,3 +21,31 @@ class FdaAltimeterControl(tk.Toplevel):
         # Set minimal size
         self.minsize(640, 480)
         self.resizable(False, False)
+
+        # Available actions on the left.
+        upload = tk.Button(self, text=_(u'Upload data'),
+                command=self.upload)
+        upload.grid(column=0, row=0, sticky='EW')
+
+        erase = tk.Button(self, text=_(u'Erase data'),
+                command=self.erase)
+        erase.grid(column=0, row=1, sticky='EW')
+
+        setup = tk.Button(self, text=_(u'Set sampling frequency'),
+                command=self.set_frequency)
+        setup.grid(column=0, row=2)
+
+    def upload(self):
+        tkMessageBox.showwarning('Upload flight data', 'Not implemented yet')
+
+    def erase(self):
+        reply = tkMessageBox.askokcancel(_(u'Erase flight data'),
+                _(u'Really erase all flight data in your altimeter?'))
+        if reply:
+            self.do_erase()
+
+    def do_erase(self):
+        tkMessageBox.showwarning('Erase flight data', 'Not implemented yet')
+
+    def set_frequency(self):
+        tkMessageBox.showwarning('Upload flight data', 'Not implemented yet')
