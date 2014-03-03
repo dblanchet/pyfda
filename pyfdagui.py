15995b2b4b7d94e032aed73ba5c356ff3eabce40
David Blanchet <david.blanchet5@gmail.com>
2014-03-03 16:16:30 +0100

    GUI: Open application when FDA files are double-clicked

--- pyfdagui.py
+++ pyfdagui.py
@@ -8,6 +8,25 @@ from gui.fdafileviewer import FdaFileViewer
 def main(argv):
     filename = argv[1] if argv[1:] else None
     app = FdaFileViewer(filename)
+
+    # Deal with Mac OS X data file double-click
+    # handling: clicked files are not given on
+    # command line, but through a system signal.
+    if sys.platform.startswith('darwin'):
+
+        def doOpenFile(*args):
+            # Open first file only.
+            for fname in args:
+                app.load_file(fname)
+                break
+
+        # The command below is a hook in aquatk that is called whenever
+        # the app receives a file open event. The callback can have
+        # multiple arguments, one for every file that should be opened.
+        #
+        # http://stackoverflow.com/a/1004487
+        app.createcommand("::tk::mac::OpenDocument", doOpenFile)
+
     app.mainloop()
     return 0
 
