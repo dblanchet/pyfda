b29b8d3afb98a6c4f0d2bd85a26172857b6750b5
David Blanchet <david.blanchet5@gmail.com>
2014-03-31 23:48:55 +0200

    CLI: Add more unit tests

--- tests/test_pyfda.py
+++ tests/test_pyfda.py
@@ -5,19 +5,25 @@ import os
 from unittest import TestCase
 from scripttest import TestFileEnvironment
 
+TEST_SUBDIR = 'tests/test-output'
+TEST_DIR = os.path.abspath(TEST_SUBDIR)
+TEMPLATE_DIR = os.path.abspath('tests')
+
+INPUT_FILE_NAME = 'sample.fda'
+INPUT_FILE_PATH = os.path.join(TEST_SUBDIR, INPUT_FILE_NAME)
+CONVERTED_PREFIX = os.path.join(TEST_SUBDIR, 'test_convert')
+
 
 class TestPyfdaCommandLineTool(TestCase):
 
     def setUp(self):
         self.env = TestFileEnvironment(
-                base_path=os.path.abspath('tests/test-output'),
+                base_path=TEST_DIR,
+                template_path=TEMPLATE_DIR,
                 cwd=os.getcwd(),
-                #environ={
-                    #'PATH': '/usr/local/bin:/usr/bin:/bin',
-                    #'HOME': os.path.abspath('tests/test-output'),
-                    #},
-                #template_path=os.path.abspath('tests/data'),
                 ignore_hidden=False)
+        self.env.writefile(INPUT_FILE_NAME,
+                frompath='test_freq_2_then_1_then_8_then_4.fda')
 
     def test_no_arguments(self):
         'Test: pyfda.py'
@@ -112,4 +118,79 @@ class TestPyfdaCommandLineTool(TestCase):
         expected = 'error: argument --last: invalid int value:'
         self.assertTrue(expected in result.stderr, 'Incorrect error message')
 
-# vim:nosi:
+    def test_print_fda_file_info(self):
+        'Test: pyfda info sample.fda'
+
+        cmd = './pyfda.py info ' + INPUT_FILE_PATH
+        result = self.env.run(cmd, expect_error=False)
+
+        self.assertEqual(result.returncode, 0,\
+                'Expect returncode=0, got %r' % result.returncode)
+        expected = \
+"""Reading file %s...
+Found 4 flights:
+   0:      216 records @ 2Hz -   108.000 seconds
+   1:      136 records @ 1Hz -   136.000 seconds
+   2:      496 records @ 8Hz -    62.000 seconds
+   3:      176 records @ 4Hz -    44.000 seconds
+""" % INPUT_FILE_PATH
+        self.assertEqual(expected, result.stdout, 'Incorrect output')
+
+    def test_print_fda_file_convert_all_csv(self):
+        'Test: pyfda --prefix=test_convert convert sample.fda'
+
+        cmd = './pyfda.py --prefix=%s convert %s' % (
+                CONVERTED_PREFIX, INPUT_FILE_PATH)
+        result = self.env.run(cmd, expect_error=False)
+
+        self.assertEqual(result.returncode, 0,\
+                'Expect returncode=0, got %r' % result.returncode)
+        expected = \
+"""Reading file %s...
+Found 4 flights:
+Converting 4 flight(s) to CSV...
+   Writing %s_000.csv file
+   Writing %s_001.csv file
+   Writing %s_002.csv file
+   Writing %s_003.csv file
+""" % (INPUT_FILE_PATH, CONVERTED_PREFIX, CONVERTED_PREFIX,
+        CONVERTED_PREFIX, CONVERTED_PREFIX)
+        self.assertEqual(expected, result.stdout, 'Incorrect output')
+        # TODO: Check converted file contents.
+
+    def test_print_fda_file_convert_last_csv(self):
+        'Test: pyfda --last --prefix=test_convert convert sample.fda'
+
+        cmd = './pyfda.py --last --prefix=%s convert %s' % (
+                CONVERTED_PREFIX, INPUT_FILE_PATH)
+        result = self.env.run(cmd, expect_error=False)
+
+        self.assertEqual(result.returncode, 0,\
+                'Expect returncode=0, got %r' % result.returncode)
+        expected = \
+"""Reading file %s...
+Found 4 flights:
+Converting 1 flight(s) to CSV...
+   Writing %s_003.csv file
+""" % (INPUT_FILE_PATH, CONVERTED_PREFIX)
+        self.assertEqual(expected, result.stdout, 'Incorrect output')
+        # TODO: Check converted file content.
+
+    def test_print_fda_file_convert_last_two_csv(self):
+        'Test: pyfda --last=2 --prefix=test_convert convert sample.fda'
+
+        cmd = './pyfda.py --last=2 --prefix=%s convert %s' % (
+                CONVERTED_PREFIX, INPUT_FILE_PATH)
+        result = self.env.run(cmd, expect_error=False)
+
+        self.assertEqual(result.returncode, 0,\
+                'Expect returncode=0, got %r' % result.returncode)
+        expected = \
+"""Reading file %s...
+Found 4 flights:
+Converting 2 flight(s) to CSV...
+   Writing %s_002.csv file
+   Writing %s_003.csv file
+""" % (INPUT_FILE_PATH, CONVERTED_PREFIX, CONVERTED_PREFIX)
+        self.assertEqual(expected, result.stdout, 'Incorrect output')
+        # TODO: Check converted file contents.
